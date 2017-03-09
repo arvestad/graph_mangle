737b3a53fadb0b270499ec8020760d74fc326c60
arvestad <arve@csc.kth.se>
2017-03-09 13:34:06 +0100

    Updated for 'new' JSON format and added .lst output as option.

--- extract_components.py
+++ extract_components.py
@@ -13,6 +13,7 @@ def main():
     parser.add_argument("component_file", help="Input file containing components in JSON format.")
     parser.add_argument("-m", "--minsize", type=int, help="Minimum component size")
     parser.add_argument("-M", "--maxsize", type=int, help="Maximum component size")
+    parser.add_argument("-l", "--lst", action="store_true", help="Use the simple format of one contig id per line, instead of the default JSON output. Output goes to files named 'component_<num>.lst', where <num> is an integer.")
     args=parser.parse_args()
 
     if args.minsize:
@@ -26,15 +27,24 @@ def main():
         data = json.load(ih)
         components = data['components']
         for c in components:
-            sz = len(c)
+            sz = len(c['nodes'])
             if sz >= minsize and sz <= maxsize:
                 selected_components.append(c)
 
-    output = { "graph_file" : "selected from " + args.component_file,
-               "components" : selected_components
-    }
-
-    print(json.dumps(output, indent=4))
+    if args.lst:
+        i = 0
+        for c in selected_components:
+            filename = 'component_' + str(i) + '.lst'
+            with open(filename, "w") as oh:
+                for contig in c['nodes']:
+                    oh.write(contig)
+                    oh.write("\n")
+            i += 1
+    else:
+        output = { "graph_file" : "selected from " + args.component_file,
+                   "components" : selected_components
+        }
+        print(json.dumps(output, indent=4))
 
 if __name__ == '__main__':
     main()
